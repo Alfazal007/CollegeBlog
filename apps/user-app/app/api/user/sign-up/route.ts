c34c0e9a094e581ecc32fff199a5be7963a0fc91
Alfazal007 <alfazal7777@gmail.com>
2024-04-27 15:24:12 +0530

    feat: signup schema with added package

--- apps/user-app/app/api/user/sign-up/route.ts
+++ apps/user-app/app/api/user/sign-up/route.ts
@@ -1,16 +1,49 @@
 import prisma from "@repo/db/client";
 import { ApiResponse } from "../../../../lib/ApiResponse";
 import { ApiError } from "../../../../lib/ApiError";
+import { signUpSchema } from "@repo/zod/schema";
+import bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
     const { username, email, password } = await request.json();
-    console.log({ username });
     if (!username || !email || !password) {
         return Response.json(new ApiError(400, "Incomplete data given"), {
             status: 400,
         });
     }
-    return Response.json(new ApiResponse(200, "Complete data given", {}), {
+    const isValid = signUpSchema.safeParse({
+        username,
+        email,
+        password,
+    });
+    if (!isValid.success) {
+        const errors = isValid.error.issues;
+        let str = "";
+        errors.forEach((err) => (str += `${err.message}  `));
+        return Response.json(new ApiError(400, str), {
+            status: 400,
+        });
+    }
+    const indexOfAt = email.indexOf("@");
+    const indexOfDot = email.indexOf(".");
+    if (indexOfAt == -1 || indexOfDot == -1 || indexOfAt > indexOfDot) {
+        return Response.json(new ApiError(400, "Invalid email given"), {
+            status: 400,
+        });
+    }
+    const collegeName = email.substring(indexOfAt + 1, indexOfDot);
+    if (collegeName.length <= 3) {
+        return Response.json(
+            new ApiError(400, "Invalid email given or not of a college domain"),
+            {
+                status: 400,
+            }
+        );
+    }
+    // TODO:: password hashing algorithm
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // TODO:: verify code and send email
+    return Response.json(new ApiResponse(200, "Valid email given", {}), {
         status: 200,
     });
 }
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,3 +2,4 @@ export default function Page(): JSX.Element {
     return <div className="text-2xl font-bold">Hello world</div>;
 }
 // docker exec -it  <container-name> psql -U postgres postgres
+// npx prisma migrate dev --name init
